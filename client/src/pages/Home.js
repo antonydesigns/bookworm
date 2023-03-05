eb85b68f0cae52c82b1fa8deec68786bc5b37c60
gio <gio>
2023-03-05 15:00:42 +0800

    change grid to flexbox

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
 import axios from "axios";
 
 function Home() {
@@ -23,11 +22,11 @@ function Home() {
   return (
     <div>
       <div className="books mt-5">
-        <div className="cards grid grid-cols-3">
+        <div className="cards flex  justify-center content-center">
           {books.map((book) => (
             <Link
               key={book.id}
-              className="card border border-black rounded-lg text-center mx-5 mt-5 block bg-blue-200 hover:bg-blue-100"
+              className="w-full card border border-black rounded-lg text-center gap-12 block bg-blue-200 hover:bg-blue-100"
             >
               <p className="title">{book.title}</p>
               <p className="rating">{book.rating}</p>
