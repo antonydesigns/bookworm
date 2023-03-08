1c9080dcaa39ba7b1ed90237ab1ab212dfbf523c
gio <gio>
2023-03-08 12:39:03 +0800

    first attempt at incorporating sketch.css

--- client/src/pages/NewBook.js
+++ client/src/pages/NewBook.js
@@ -1,5 +1,41 @@
+import { useState, useEffect } from "react";
+
 function NewBook() {
-  return <div>NewBook</div>;
+  const [newBook, setNewBook] = useState({
+    title: "",
+    rating: "",
+    cover: "",
+  });
+
+  const handleChange = (e) => {
+    setNewBook((inputs) => ({ ...inputs, [e.target.name]: e.target.value }));
+    console.log(newBook);
+  };
+
+  return (
+    <div>
+      <form className="">
+        <input
+          type="text"
+          placeholder="title"
+          onChange={handleChange}
+          name="title"
+        />
+        <input
+          type="text"
+          placeholder="rating"
+          onChange={handleChange}
+          name="rating"
+        />
+        <input
+          type="text"
+          placeholder="cover"
+          onChange={handleChange}
+          name="cover"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default NewBook;
